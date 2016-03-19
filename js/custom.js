1060e9533642ddd7cdc37f9e031c4aa28689c303
maxdonetsk <max.donetsk@mail.ru>
2016-03-19 19:21:23 +0200

    done

--- js/custom.js
+++ js/custom.js
@@ -3,30 +3,27 @@ $(document).ready(function () {
     var switcher = $("#infinite-scroll");
     var content = $('#content');
 
-    //stop lazy load if true
-    var stopLazyLoad = false;
-
     //bootstrap-switch initialization
-    switcher.bootstrapSwitch('state', false, true);
-
-    //making switcher small-sized
     switcher.bootstrapSwitch({
-        size: 'small'
+        state: false
     });
 
-    //hide loadmore button if switcher is turned on
+    //hide loadmore button when switcher is on
     switcher.on('switchChange.bootstrapSwitch', function (event, state) {
-        state ? loadmore.fadeOut('fast') : loadmore.fadeIn('fast');
-    });
+        if (state) {
+            loadmore.fadeOut('fast');
+            lazyLoad();
+            infiniteScroll();
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop() === $(document).height() - $(window).height()) {
-            runLazyLoad();
+        } else {
+            loadmore.fadeIn('fast');
+            $(window).unbind('scroll'); //this shit works
+            return; //this shit doesn't work
         }
     });
 
     loadmore.on('click', function () {
-        runLazyLoad();
+        lazyLoad();
     });
 
     function lazyLoad() {
@@ -35,16 +32,17 @@ $(document).ready(function () {
             cache: false,
             success: function (data) {
                 $.each(data, function (index, value) {
-                    if ((index + 1) === data.length) {
-                        stopLazyLoad = true;
-                    }
                     $.get('/template/template.html', function (data) {
                         data = $.parseHTML(data);
                         data.filter(function (data) {
                             $(data).find('img').attr('src', value.image);
                             $(data).find('.title').html(value.title);
                             $(data).find('.description').html(value.paragraph);
+                            for (var i = 0; i < value.rating; i++) {
+                                $(data).find('.rating > span').eq(i).addClass('active');
+                            }
                             if ($(data).is('[class^="col-"]')) {
+                                //it's better to implemented this feature in css, but i'm lazy
                                 var n = 3; //each nth element; equals to column number
                                 if (index % n === 0) {
                                     data.className = data.className + ' text-center text-md-right';
@@ -56,25 +54,22 @@ $(document).ready(function () {
                                     data.className = data.className + ' text-center';
                                 }
                             }
+                            content.last().append(data);
                         });
-                        content.last().append(data);
                     });
                 });
             },
             error: function () {
                 alert('Error!');
-            },
-            complete: function () {
-                console.log('complete');
             }
         });
     }
 
-    function runLazyLoad() {
-        if (stopLazyLoad) {
-            loadmore.remove();
-            return;
-        }
-        lazyLoad();
+    function infiniteScroll() {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() === $(document).height() - $(window).height()) {
+                lazyLoad();
+            }
+        });
     }
 });
